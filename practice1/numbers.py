af4d6d5a2ca1892a4c2b45aa82a12cb12fa5b4bc
NNasipkali <nursmurf11@gmail.com>
2026-01-24 22:19:02 +0500

    Add numbers example

---
+++ practice1/numbers.py
@@ -0,0 +1,24 @@
+#1
+x = 1
+print(x)
+print(type(x))
+#2 
+x = 1.10
+print(x)
+print(type(x))
+#3 
+x = 3j 
+print(x)
+print(type(x))
+#4 
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+#5 
+import random
+
+print(random.randrange(1, 10))
