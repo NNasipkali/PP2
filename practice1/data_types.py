cd4e8a0a7490a84f842790810da8417cd47cce8a
NNasipkali <nursmurf11@gmail.com>
2026-01-24 22:34:48 +0500

    Add data types examples

---
+++ practice1/data_types.py
@@ -0,0 +1,30 @@
+#1 
+x = 1
+print(type(x))
+#2 
+s = "Hello"
+print(type(s))
+#3
+f = 3.14 
+print(type(f))
+#4 
+l = ["apple", "banana", "cherry"]
+print(type(l))
+#5
+b = True
+print(type(b))
+#6
+# Data types example
+
+a = 10          # int
+b = 3.14        # float
+c = "Hello"     # string
+d = True        # boolean
+e = [1, 2, 3]   # list
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
