9bf232255c46cbbf9bbbca3c63c9c72f04096f7d
NNasipkali <nursmurf11@gmail.com>
2026-01-24 21:46:18 +0500

    prac01 in progress

---
+++ practice1/strings.py
@@ -0,0 +1,17 @@
+#1 
+print("Hello World")
+#2 
+a = "Hello"
+print(a)
+#3 
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#4 
+a = "Hello World!"
+print(a[1])
+#5 
+for x in "banana":
+    print(x)
