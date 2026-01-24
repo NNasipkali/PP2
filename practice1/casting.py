0df876ba8d472682cca784a404765a04d807c53d
NNasipkali <nursmurf11@gmail.com>
2026-01-24 22:50:54 +0500

    Add casting examples

---
+++ practice1/casting.py
@@ -0,0 +1,30 @@
+#1
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+#2
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+print(x)
+print(y)
+print(z)
+print(w)
+#3
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+#4 convert string to integer
+x = "10" 
+x_int = int(x)
+print(x_int)
+#5 convert integer to string
+x_str = str(x_int)
+print(x_str)
