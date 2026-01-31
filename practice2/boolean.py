5bcc5741407e444276806e5329298b8f01d082f6
NNasipkali <nursmurf11@gmail.com>
2026-01-31 15:25:48 +0500

    Add boolean examples

---
+++ practice2/boolean.py
@@ -0,0 +1,29 @@
+#1 When you compare two values, the expression is evaluated and Python returns the Boolean answer
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#2 Print a message based on whether the condition is True or False:
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  
+#3 any non-empty string → True / empty string "" → False
+print(bool("Hello"))
+print(bool(15))
+
+#4 Evaluate two variables:
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+#5
+print(True and False)   # False
+print(True or False)    # True
+print(not True)         # False
