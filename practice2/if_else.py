7dc8ed7535253f97c650369b75ed049730d3d9f8
NNasipkali <nursmurf11@gmail.com>
2026-01-31 15:57:49 +0500

    add if else examples

---
+++ practice2/if_else.py
@@ -0,0 +1,43 @@
+#1
+number  = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+  
+#2 
+a = 10
+b = 5
+
+if a > b:
+    print("a больше b")
+else:
+    print("a не больше b")
+  
+#3 
+age = 16
+
+if age >= 18:
+    print("Доступ разрешён")
+else:
+    print("Доступ запрещён")
+
+  
+#4 
+username = "Emil"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+  
+#5 
+text = ""
+
+if text:
+    print("Строка не пустая")
+else:
+    print("Строка пустая")
+
+  
