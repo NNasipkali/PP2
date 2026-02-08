9abbab0ff8079d509b38ee8c4909c0a2de6d4e7b
NNasipkali <nursmurf11@gmail.com>
2026-02-08 17:37:23 +0500

    add basic functions examples

---
+++ practice3/functions/basic_functions.py
@@ -0,0 +1,35 @@
+#1
+def greet(): 
+    """
+    This function prints greeting message 
+    """
+    print("Hello, welcome to Python functions!")
+   
+#calling the function  
+greet()
+
+#2
+def sum(): 
+    """
+    This function prints the sum of two numbers 
+    """
+    print(5+6)
+sum()
+
+#3
+def show_program_name():
+    """
+    This function prints the program name.
+    """
+    print("Practice 03 - Basic Functions")
+show_program_name()
+#4 
+def show_list():
+    """Prints a list"""
+    print([1, 2, 3, 4])
+show_list()
+#5 
+def show_status():
+    """Prints a boolean value"""
+    print(True)
+show_status()
