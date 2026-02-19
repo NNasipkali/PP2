774c70f3fa1668b5fe2977d074afa1fc5988bc6f
NNasipkali <nursmurf11@gmail.com>
2026-02-19 14:17:39 +0500

    generators tasks solution

--- practice4/generators.py
+++ practice4/generators.py
@@ -1,39 +1,39 @@
 #1 
-# def square_numbers(N): 
-#     for i in range(1, N + 1): 
-#         yield i * i
+def square_numbers(N): 
+    for i in range(1, N + 1): 
+        yield i * i
 
-# N = int(input())
-# for value in square_numbers(N): 
-#     print(value)
+N = int(input())
+for value in square_numbers(N): 
+    print(value)
 
-#2 
-# def even_numbers(N): 
-#     for i in range(N+1): 
-#         if i % 2 == 0: 
-#             yield i
+2 
+def even_numbers(N): 
+    for i in range(N+1): 
+        if i % 2 == 0: 
+            yield i
 
-# N = int(input())
-# print(", ".join(str(x) for x in even_numbers(N)))
+N = int(input())
+print(", ".join(str(x) for x in even_numbers(N)))
 
 #3 
-# def div_by_3_4(N): 
-#     for i in range(N+1):
-#         if i % 3 == 0 and i % 4 == 0: 
-#             yield i
+def div_by_3_4(N): 
+    for i in range(N+1):
+        if i % 3 == 0 and i % 4 == 0: 
+            yield i
             
-# N = int(input())
-# for value in div_by_3_4(N): 
-#     print(value)
+N = int(input())
+for value in div_by_3_4(N): 
+    print(value)
 
 #4 
-# def squares(a, b): 
-#     for i in range(a, b+1): 
-#         yield i * i
+def squares(a, b): 
+    for i in range(a, b+1): 
+        yield i * i
         
-# a, b = map(int, input().split())
-# for value in squares(a, b):
-#     print(value)
+a, b = map(int, input().split())
+for value in squares(a, b):
+    print(value)
 
 #5 
 def down(N): 
